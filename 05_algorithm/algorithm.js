c46335d70be7d82a1dc6fd4f1d03db455c7ccb9c
Takayuki Toyoshima <oq0q6383qqs@gmail.com>
2018-06-25 23:34:26 +0900

    Finished implemetetion

--- 05_algorithm/algorithm.js
+++ 05_algorithm/algorithm.js
@@ -1,18 +1,12 @@
-function getSum(numbers) {
-  let result = 0
-  for (const number of numbers) {
-    result += number
-  }
-  return result
-}
+'use strict'
 
-function getAverage(numbers) {
-  const sum = getSum(numbers)
-  const result = sum / numbers.length
-  return result
-}
+const numbers = [20, 31, 42, 13, 5, 38]
 
-function getMaximum(numbers) {
+const sum = numbers => numbers.reduce((a, x) => (a += x), 0)
+const average = numbers => sum(numbers) / numbers.length
+
+// もう少し簡潔にできそう
+const maximum = numbers => {
   let result = 0
   for (const number of numbers) {
     if (result < number) {
@@ -22,7 +16,8 @@ function getMaximum(numbers) {
   return result
 }
 
-function getMinimum(numbers) {
+// こっちももう少し簡潔にできそう
+const minimum = numbers => {
   let result = numbers[0]
   for (const number of numbers) {
     if (result > number) {
@@ -32,7 +27,8 @@ function getMinimum(numbers) {
   return result
 }
 
-function ascending(numbers) {
+// バブルソートで実装
+const ascending = numbers => {
   for (let i = 0; i < numbers.length - 1; i++) {
     for (let j = numbers.length - 1; j > i; j--) {
       if (numbers[j - 1] > numbers[j]) {
@@ -45,42 +41,28 @@ function ascending(numbers) {
   return numbers
 }
 
-function descending(numbers) {
-// TODO: 2018/06/12 クイックソート実装予定
-
-//   if (numbers.length < 1) {
-//     return numbers
-//   }
-
-//   const pivot = numbers[0]
-//   const left = []
-//   const right = []
-
-//   for(let i = 0; i <= numbers.length -1; i++){
-//     if (numbers[i] <= pivot){
-//       left[] += numbers[i]
-//     }else{
-//       right[] += numbers[i]
-//     }
-//   }
-
-// ひとまずバブルソートで実装、別のアルゴリズムに置き換え
-  for (let i = 0; i < numbers.length - 1; i++) {
-    for (let j = numbers.length - 1; j > i; j--) {
-      if (numbers[j - 1] < numbers[j]) {
-        const temp = numbers[j - 1]
-        numbers[j - 1] = numbers[j]
-        numbers[j] = temp
-      }
-    }
+// クイックソートで実装
+const descending = numbers => {
+  if (numbers.length < 1) {
+    return numbers
   }
-  return numbers
+
+  const pivot = numbers[0]
+  let left = []
+  let right = []
+
+  for (let i = 1; i <= numbers.length - 1; i++) {
+    numbers[i] >= pivot ? left.push(numbers[i]) : right.push(numbers[i])
+  }
+
+  left = descending(left)
+  right = descending(right)
+  return left.concat(pivot, right)
 }
 
-const numbers = [20, 31, 42, 13, 5, 38]
-console.log('合計：' + getSum(numbers))
-console.log('平均：' + getAverage(numbers))
-console.log('最大値：' + getMaximum(numbers))
-console.log('最小値：' + getMinimum(numbers))
+console.log('合計：' + sum(numbers))
+console.log('平均：' + average(numbers))
+console.log('最大値：' + maximum(numbers))
+console.log('最小値：' + minimum(numbers))
 console.log('昇順：' + ascending(numbers))
 console.log('降順：' + descending(numbers))
