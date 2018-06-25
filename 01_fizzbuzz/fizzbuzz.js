422b41b0556832ac33ce09921bb8f811392f9161
Takayuki Toyoshima <oq0q6383qqs@gmail.com>
2018-06-26 00:37:26 +0900

    made it more simply

--- 01_fizzbuzz/fizzbuzz.js
+++ 01_fizzbuzz/fizzbuzz.js
@@ -3,6 +3,5 @@
 // 三項演算子をネスト。かえって見づらいかも。。。
 const number = +window.prompt('数字を入力してください')
 for (let i = 1; i <= number; i++) {
-  const msg = i % 3 === 0 ? (i % 15 === 0 ? 'FizzBuzz' : 'Fizz') : i % 5 === 0 ? 'Buzz' : i
-  console.log(msg)
+  console.log(i % 3 === 0 ? (i % 15 === 0 ? 'FizzBuzz' : 'Fizz') : i % 5 === 0 ? 'Buzz' : i)
 }
